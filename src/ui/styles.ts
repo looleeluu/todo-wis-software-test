206fdd6dd22382a903861c09621c02af1fc73368
Ilya Sinegubkin <ilya.sinegubkin@gmail.com>
2022-09-19 11:03:04 +0300

    fix: remove space-around from container

--- src/ui/styles.ts
+++ src/ui/styles.ts
@@ -17,7 +17,6 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
   padding: 20px;
   height: 100%;
   width: 100%;
